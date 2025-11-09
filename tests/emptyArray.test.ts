8c198704a14df41032aa40a51b7f9b91cc2560fd
Prihhan <prihhan@gmail.com>
2025-11-09 20:46:26 +0200

    response code changed from numeric to literal in emptyArray test

--- tests/emptyArray.test.ts
+++ tests/emptyArray.test.ts
@@ -1,10 +1,11 @@
 import {expect, test} from "@playwright/test";
+import {StatusCodes} from "http-status-codes";
 let baseURL: string = 'http://localhost:3000/users';
 
 test('all users: should return empty array when no users', async ({ request }) => {
-    const response = await request.get(`${baseURL}`);
-    expect(response.status()).toBe(200);
-    const responseBody = await response.text()
+    const getResponse = await request.get(`${baseURL}`);
+    expect(getResponse.status()).toBe(StatusCodes.OK);
+    const responseBody = await getResponse.text()
     expect(responseBody).toBe('[]');
 });
 
